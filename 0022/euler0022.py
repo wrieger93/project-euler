c11453c4ed1d6db39e0ad1cc819451733304b84e
William Rieger <wrieger93@gmail.com>
2015-06-11 22:18:28 -0400

    Fix problem 22.

--- 0022/euler0022.py
+++ 0022/euler0022.py
@@ -1,18 +1,19 @@
-"""
-i hope this is obvious (for the most part, some parts are kinda condensed)
-"""
+# This should be self-explanatory.
 
-from functools import reduce
+# Output: 871198282
 
-def namescore(name):
+import functools
+import os
+import sys
+
+def partial_name_score(name):
     # the 64 is because ord('A') = 65
-    # the reduce just takes the product
-    return reduce(lambda a,b: a+b, [ord(x)-64 for x in name])
+    return sum([ord(x)-64 for x in name])
 
 if __name__=="__main__":
-    f = open('names.txt','r')
-    line = f.readline()
-    f.close()
+    line = None
+    with open(os.path.join(os.path.dirname(sys.argv[0]), "names.txt")) as f:
+        line = f.readline()
 
     names = sorted([x[1:-1] for x in line.split(',')])
-    print(sum([(i+1)*namescore(names[i]) for i in range(len(names))]))
+    print(sum([(i+1)*partial_name_score(names[i]) for i in range(len(names))]))
