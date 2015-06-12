9522fe73ddb23e1ef22e3975549b7f6d8f60607b
William Rieger <wrieger93@gmail.com>
2015-06-11 22:01:10 -0400

    Fix problem 18.

--- 0018/euler0018.py
+++ 0018/euler0018.py
@@ -1,18 +1,20 @@
-"""
-uses dynamic programming from the bottom up. explained better later in the
-program.
-"""
+# The DP is explained below.
 
-def loadarray():
-    f = open('triangle.txt','r')
+# Output: 1074
+
+import os
+import sys
+
+def loadarray(filename):
     array = []
-    for line in f:
-        array.append([int(x) for x in line.split()])
+    with open(filename) as f:
+        for line in f:
+            array.append([int(x) for x in line.split()])
 
     return array
 
 """
-starts from the bottom up. if a max sum goes through an element in the second-
+Starts from the bottom up. If a max sum goes through an element in the second-
 to-last row, the next element is clearly going to be the max of the two below
 it. so we can "remove" the last row and make the second-to-last row the new
 last row, where the new element is the old element plus the max of the element
@@ -20,7 +22,7 @@ beneath it. we can then do the same for the third row from the bottom, etc,
 up until the top row.
 """
 def findmax(array):
-    for row in range(len(array)-2,-1,-1):
+    for row in range(len(array)-2, -1, -1):
         for element in range(len(array[row])):
             array[row][element] += max(array[row+1][element],
                                        array[row+1][element+1])
@@ -29,5 +31,4 @@ def findmax(array):
        
 
 if __name__=="__main__":
-    print(findmax(loadarray())) # answer is 1074
-    
+    print(findmax(loadarray(os.path.join(os.path.dirname(sys.argv[0]), "triangle.txt"))))
